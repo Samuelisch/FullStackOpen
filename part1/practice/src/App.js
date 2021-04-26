3feab1002fd943cbdb45d931a87f09774c482c9f
Samuel Chan <40135391+Samuelisch@users.noreply.github.com>
2021-04-26 19:26:10 +0800

    Update App.js

--- part1/practice/src/App.js
+++ part1/practice/src/App.js
@@ -1,48 +1,12 @@
 import React, { useState } from 'react'
 
-const History = ({allClicks}) => {
-  if (!allClicks.length) {
-    return (
-      <div>
-        The app is waiting for a button press
-      </div>
-    )
-  }
-  return (
-    <div>
-      You've clicked on: {allClicks.join(' ')}
-    </div>
-  )
-}
-
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(['L'].concat(allClicks))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(['R'].concat(allClicks))
-    setRight(right + 1)
-  }
-
-  const Button = ({handleClick, text}) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
+  const [value, setValue] = useState(10)
 
   return (
     <div>
-      {left}{' '}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {' '}{right}
-      <History allClicks = {allClicks}/>
+      {value}
+      <button>reset to zero</button>
     </div>
   )
 }
