0a54b2273a23973c4ffcf1e296861640b77ae202
Samuel Chan <40135391+Samuelisch@users.noreply.github.com>
2021-04-27 22:40:05 +0800

    Parts 1.6 - 1.11

--- part1/1.6/src/App.js
+++ part1/1.6/src/App.js
@@ -1,26 +1,55 @@
 import React, { useState } from 'react'
 
+//button function to handle all three buttons, and their handle
 const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>{text}</button>
+  <button onClick={handleClick}>
+    {text}
+  </button>
 )
 
-const Display = props => (
-  <div>{props.value}</div>
-)
+const Statistic = ({text, value}) => {
+  return(
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  let all = props.good + props.neutral + props.bad
+  if (all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  return(
+    <div>
+      <Statistic text='Good' value={props.good} />
+      <Statistic text='Neutral' value={props.neutral} />
+      <Statistic text='Bad' value={props.bad} />
+      <Statistic text='All' value={all} />
+      <Statistic text='Average' value={props.points / all}/>
+      <Statistic text='Positive' value={props.good / all * 100 + '%'}/>
+    </div>
+  )
+}
 
 const App = () => {
-  const [value, setValue] = useState(10)
-
-  const setToValue = (value) => {
-      setValue(value)
-    }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [points, setPoints] = useState(0)
 
   return (
     <div>
-      <Display value={value}/>
-      <Button handleClick={() => setToValue(value + 1)} text='plus' />
-      <Button handleClick={() => setToValue(value - 1)} text='minus' />
-      <Button handleClick={() => setToValue(value * 2)} text='times2' />
+      <h1>Feedback Form</h1>
+      <Button handleClick={() => {setGood(good + 1); setPoints(points + 1)}} text={'Good'}/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text={'Neutral'}/>
+      <Button handleClick={() => {setBad(bad + 1); setPoints(points - 1)}} text={'Bad'}/>
+      <h2>Statistics:</h2>
+      <Statistics good={good} bad={bad} neutral={neutral} points={points}/>
     </div>
   )
 }
