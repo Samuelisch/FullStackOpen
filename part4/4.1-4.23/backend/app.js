d5705525ae13dbe115249c9338ee6deeed829c43
Samuelisch <samuelchanwk@hotmail.com>
2021-12-28 21:59:39 +0800

    refactored code to make it more modular, cleaner

--- part4/4.1-4.23/backend/app.js
+++ part4/4.1-4.23/backend/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const config = require('./utils/config');
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger');
-const Blog = require('./models/blog');
+const blogsRouter = require('./controllers/blogs')
 
 const app = express();
 
@@ -22,25 +22,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((savedBlog) => savedBlog.toJSON())
-    .then((savedAndFormattedBlog) => {
-      response.json(savedAndFormattedBlog);
-    })
-    .catch(error => logger.error('failed to post', error.message))
-});
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
