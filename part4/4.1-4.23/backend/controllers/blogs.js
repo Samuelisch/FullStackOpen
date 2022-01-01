a00d248c763148990e54e6e65d82f743273e445e
Samuelisch <samuelchanwk@hotmail.com>
2022-01-01 10:43:54 +0800

    added jsonwebtoken to application, for user authorization

--- part4/4.1-4.23/backend/controllers/blogs.js
+++ part4/4.1-4.23/backend/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
-const User = require('../models/user')
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('../utils/config');
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const fetchedBlogs = await Blog
@@ -20,8 +30,13 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, config.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid!' })
+  }
+  
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title: body.title,
     author: body.author,
